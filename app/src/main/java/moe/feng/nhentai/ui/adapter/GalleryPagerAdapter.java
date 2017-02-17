e7d4fca1b3d2f03be3ce1040f3b99a4f693adc54
ruby-roo <babyboy28ruby@gmail.com>
2017-02-16 23:59:04 -0500

    User proper Fragment manager

--- app/src/main/java/moe/feng/nhentai/ui/adapter/GalleryPagerAdapter.java
+++ app/src/main/java/moe/feng/nhentai/ui/adapter/GalleryPagerAdapter.java
@@ -2,7 +2,6 @@ package moe.feng.nhentai.ui.adapter;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.os.Handler;
 import android.support.v13.app.FragmentPagerAdapter;
 
 import moe.feng.nhentai.model.Book;
@@ -10,49 +9,48 @@ import moe.feng.nhentai.ui.fragment.BookPageFragment;
 
 public class GalleryPagerAdapter extends FragmentPagerAdapter {
 
-	private Book book;
-	private BookPageFragment[] fragments;
+	private Book mBook;
+	private BookPageFragment[] mFragments;
+	private FragmentManager mFragmentManager;
 
-	public GalleryPagerAdapter(FragmentManager fm, Book book) {
-		super(fm);
-		this.book = book;
-		this.fragments = new BookPageFragment[book.pageCount];
+	public GalleryPagerAdapter(FragmentManager fragmentManager, Book book) {
+		super(fragmentManager);
+		mFragmentManager = fragmentManager;
+		mBook = book;
+		mFragments = new BookPageFragment[book.pageCount];
 	}
 
 	@Override
 	public Fragment getItem(int position) {
-		if(position<0|| position>=book.pageCount){
+		if (position < 0 || position >= mBook.pageCount) {
 			return null;
 		}
 
-		if (fragments[position] == null) {
-			fragments[position] = BookPageFragment.newInstance(book, position + 1);
+		if (mFragments[position] == null) {
+			mFragments[position] = BookPageFragment.newInstance(mBook, position + 1);
 		}
 
-		return fragments[position];
+		return mFragments[position];
 	}
 
 	@Override
 	public int getCount() {
-		return book.pageCount;
+		return mBook.pageCount;
 	}
 
-	public void eraseItem(int position){
-		if (fragments[position]!=null){
-			fragments[position].getFragmentManager().beginTransaction().remove(fragments[position]).commit();
-			fragments[position]=null;
+	public void eraseItem(int position) {
+		BookPageFragment fragment = mFragments[position];
+		if (fragment != null) {
+			mFragmentManager.beginTransaction().remove(fragment).commit();
+			mFragments[position] = null;
 		}
-
-
 	}
 
 	public void notifyPageImageLoaded(int position, boolean isSucceed) {
-		if (fragments[position] != null) {
-			Handler handler = fragments[position].getHandler();
-			if (handler != null) {
-				handler.sendEmptyMessage(
-						isSucceed ? BookPageFragment.MSG_FINISHED_LOADING : BookPageFragment.MSG_ERROR_LOADING
-				);
+		BookPageFragment fragment = mFragments[position];
+		if (fragment != null && fragment.getHandler() != null) {
+			if (fragment.getHandler() != null) {
+				fragment.getHandler().sendEmptyMessage(isSucceed ? BookPageFragment.MSG_FINISHED_LOADING : BookPageFragment.MSG_ERROR_LOADING);
 			}
 		}
 	}
